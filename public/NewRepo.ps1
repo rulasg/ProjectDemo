996517a34be594136056798d5960c481953cb46d
rulasg <rulasg@outlook.com>
2023-11-07 09:52:22 +0100

    New-Repo with projectdemo default topic

--- public/NewRepo.ps1
+++ public/NewRepo.ps1
@@ -7,10 +7,15 @@ function New-Repo{
 
     process {
         "Creating repo [$RepoWithOwner] with topic [$Topic]" | Write-Verbose
+
+        $commad = 'gh repo create {repowithowner} --add-readme --public --description "Repo part of Project Demo"'
+        $commad = $commad -replace "{repowithowner}",$RepoWithOwner
         
-        # create repo
-        "[New-Repo] gh repo create $RepoWithOwner --add-readme --public --description `"Repo fart of Project Demo`"" | Write-Verbose
-        gh repo create $RepoWithOwner --add-readme --public --description "Repo fart of Project Demo"
+        $commad | Write-Verbose
+
+        $result = Invoke-Expression $commad
+
+        $result
         
         Add-TopicToRepo -RepoWithOwner $RepoWithOwner -Topic $Topic
         Add-TopicToRepo -RepoWithOwner $RepoWithOwner -Topic projectdemo
@@ -24,6 +29,9 @@ function Add-TopicToRepo{
         [Parameter(Position=0,ValueFromPipeline)][string]$RepoWithOwner,
         [Parameter(Position=1)][string]$Topic
     )
+
+    "Adding topic [$Topic] to repo [$RepoWithOwner]" | Write-Verbose
+
     # add topic for easy find
     $command = 'gh repo edit {repowithowner} --add-topic "{topic}"'
     $command = $command -replace "{repowithowner}",$RepoWithOwner
