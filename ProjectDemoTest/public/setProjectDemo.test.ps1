d0608be14c74b8bdf1354b4123502f1f23d32636
rulasg <rulasg@outlook.com>
2023-11-15 19:35:09 +0100

    set-projectdemo

---
+++ ProjectDemoTest/public/setProjectDemo.test.ps1
@@ -0,0 +1,8 @@
+function ProjectDemoTest_ProjectDemo_Set{
+
+    Assert-SkipTest
+
+    # $result = Set-Projectdemo
+
+    Assert-NotImplemented
+}
---
+++ public/setProjectDemo.ps1
@@ -0,0 +1,28 @@
+function Set-ProjectDemo{
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter()] [string]$Name,
+        [Parameter()] [string]$Owner
+    )
+
+    $env = Get-Environment -Name $Name -Owner $Owner
+    $name = $env.Name
+    $owner = $env.Owner
+    
+    New-ProjectDemo     -name $name  -Owner $owner
+    
+    Add-ItemsToProject  -name $name  -Owner $owner
+
+    $projectNumber = Get-ProjectNumber -Name $name -Owner $owner
+    
+    Update-FieldValueSingleSelectToProject      -ProjectNumber $projectNumber -owner $owner -FieldName "Status"
+    
+    Add-FieldSingleSelectToProject              -ProjectNumber $projectNumber -owner $owner -FieldName "Priority" -Options "🔥Critical,🥵High,😊Normal,🥶Low"
+    Update-FieldValueSingleSelectToProject      -ProjectNumber $projectNumber -owner $owner -FieldName "Priority"
+    
+    Add-FieldSingleSelectToProject              -ProjectNumber $projectNumber -owner $owner -FieldName "Severity" -Options "Critical⭐️⭐️⭐️⭐️,Important⭐️⭐️⭐️,Needed⭐️⭐️,Nice⭐️" 
+    Update-FieldValueSingleSelectToProject      -ProjectNumber $projectNumber -owner $owner -FieldName "Severity"
+
+    Add-TimeTrackerToProject                    -ProjectNumber $projectNumber -owner $owner 
+
+} Export-ModuleMember -Function Set-ProjectDemo
