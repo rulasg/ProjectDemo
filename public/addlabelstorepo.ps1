6b023b0d74319afbc13b3f10773f72beb9dcb123
rulasg <rulasg@outlook.com>
2023-12-01 20:51:24 +0100

    avoid recreating labels when they exist Adding to labels more to wellknown Fixes #32

--- public/addlabelstorepo.ps1
+++ public/addlabelstorepo.ps1
@@ -1,57 +1,105 @@
+
+function Test-LabelToRepo{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)][string]$Repo,
+        [Parameter(Mandatory)][string]$Name,
+        [Parameter()][string]$Owner
+    )
+
+    process {
+
+        $Owner = Get-EnvironmentOwner -Owner $Owner
+
+        $command = 'gh label list -R rulasg/testpublicrepo  --json name | convertfrom-json '
+        
+        $command = 'gh label list -R {owner}/{repo} --json name -S "{name}"'
+        $command = $command -replace "{owner}", $Owner
+        $command = $command -replace "{repo}", $Repo
+        $command = $command -replace "{name}", $Name
+        
+        $command | Write-Verbose
+        
+        $resultJson = Invoke-Expression $command
+
+        $result = $resultJson | ConvertFrom-Json | select-object -ExpandProperty name
+
+        return $result -eq $Name
+    }
+} Export-ModuleMember -Function Test-LabelToRepo
+
 function Add-LabelToRepo{
     [CmdletBinding(SupportsShouldProcess)]
     param(
-        [Parameter(Mandatory)] [string]$Repo,
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)] [string]$Repo,
         [Parameter(Mandatory)] [string]$Name,
         [Parameter()] [string]$Owner,
         [Parameter()] [string]$Description,
-        [Parameter()] [string]$Color
+        [Parameter()] [string]$Color,
+        [Parameter()] [switch]$Force
     )
 
-    $Owner = Get-EnvironmentOwner -Owner $Owner
+    process{
 
-    $command = 'gh label create {name} -R {owner}/{repo} --force'
-    $command = $command -replace "{name}", $Name
-    $command = $command -replace "{owner}", $Owner
-    $command = $command -replace "{repo}", $Repo
+        if(-not$Force){
+            $exists = Test-LabelToRepo -Owner $Owner -Repo $Repo -Name $Name
+            if($exists){
+                Write-MyVerbose "Label [$Name] already exists in repo [$Repo]"
+                return
+            }
+        }
 
-    
-    if(-not[string]::IsNullOrWhiteSpace($Description)){
-        $command = $command + ' --description "{description}"'
-        $command = $command -replace "{description}", $Description
+        $Owner = Get-EnvironmentOwner -Owner $Owner
+        
+        $command = 'gh label create "{name}" -R {owner}/{repo} --force'
+        $command = $command -replace "{name}", $Name
+        $command = $command -replace "{owner}", $Owner
+        $command = $command -replace "{repo}", $Repo
+        
+        
+        if(-not[string]::IsNullOrWhiteSpace($Description)){
+            $command = $command + ' --description "{description}"'
+            $command = $command -replace "{description}", $Description
+        }
+        
+        if(-not[string]::IsNullOrWhiteSpace($Color)){
+            $command = $command + ' --color "{color}"'
+            $command = $command -replace "{color}", $Color
+        }
+        
+        $command | Write-MyVerbose
+        
+        $result = Invoke-Expression $command
+        
+        return $result
     }
-
-    if(-not[string]::IsNullOrWhiteSpace($Color)){
-        $command = $command + ' --color "{color}"'
-        $command = $command -replace "{color}", $Color
-    }
-
-    $command | Write-MyVerbose
-
-    $result = Invoke-Expression $command
-
-    return $result
 } Export-ModuleMember -Function Add-LabelToRepo
 
 function Add-WellknownLabelsToRepos{
     [CmdletBinding(SupportsShouldProcess)]
     param(
-        [Parameter(Mandatory)][string]$Repo,
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)][string]$Repo,
         [Parameter()][switch]$Update,
         [Parameter()][string]$Owner
     )
 
-    $env = Get-EnvironmentOwner -Owner $Owner 
+    process{
 
-    $labels = @(
-        @{name="epic";    description="Initiative big in size and impact";                       color="B60205"}
-        @{name="feature"; description="Initiative part of an epic with relative entity";         color="D93F0B"}
-        @{name="task";    description="small unit of work";                                      color="FBCA04"}
-    )
-
-    $labels | ForEach-Object{
-
-        Add-LabelToRepo -Owner $Owner -Repo $Repo -Name $_.name -Description $_.description -Color $_.color
+        $env = Get-EnvironmentOwner -Owner $Owner 
+        
+        $labels = @(
+            @{name="epic";    description="Initiative big in size and impact";                       color="B60205"}
+            @{name="feature"; description="Initiative part of an epic with relative entity";         color="D93F0B"}
+            @{name="task";    description="small unit of work";                                      color="FBCA04"}
+            @{name="➰ recursive"; description="task that will run several times";                   color="D4C5F9"}
+            @{name="follow-up"; description="nothing to be done. Follow-up how evolves";             color="C2E0C6"}
+            )
+            
+            $labels | ForEach-Object{
+                
+                Add-LabelToRepo -Owner $Owner -Repo $Repo -Name $_.name -Description $_.description -Color $_.color
+            }
+            "Ended adding wellknown labels to repo [$Repo]" | Write-MyVerbose -NewLine
     }
 
 } Export-ModuleMember -Function Add-WellknownLabelsToRepos
