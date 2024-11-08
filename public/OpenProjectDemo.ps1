efdaf54b6ee1f6a6921ed1b2fbfd55ffbc45038d
rulasg <rulasg@github.com>
2024-11-08 12:29:32 +0100

    feat: Add Repos parameter to Open-ProjectDemo function for optional repository opening

--- public/OpenProjectDemo.ps1
+++ public/OpenProjectDemo.ps1
@@ -2,13 +2,16 @@ function Open-ProjectDemo{
     [CmdletBinding()]
     param(
         [Parameter()] [string]$Name,
-        [Parameter()] [string]$Owner
+        [Parameter()] [string]$Owner,
+        [Parameter()] [switch]$Repos
     )
 
     $env = Get-Environment -Name $Name -Owner $Owner
 
     Find-ProjectDemo -Owner $env.Owner | Where-Object {$_.name -eq $env.Name} | Open-Url
 
-    Find-RepoDemo -Name $env.Name -Owner $env.Owner | Open-Url
+    if ($Repos) {
+        Find-RepoDemo -Name $env.Name -Owner $env.Owner | Open-Url
+    }
 
 } Export-ModuleMember -Function Open-ProjectDemo
